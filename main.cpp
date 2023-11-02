3f3d113097a5a3ca43f8a0df8457a85a84249f08
SyrineJedidi <130938927+SyrineJedidi@users.noreply.github.com>
2023-11-02 04:24:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
-#include <iostream>
-#include <string>
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>800</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget"/>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>800</width>
+     <height>26</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
 
-using namespace std;
-
-int main
-{
-    cout<<"Hello world!!"<<endl;
-/*syrine*/
-    return 0;
-}

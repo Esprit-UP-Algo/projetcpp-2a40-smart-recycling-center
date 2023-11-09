a58d290aa9d3fb387798189efe800ad7d3a664f9
med-aziz-benamor <mohammedaziz.benamor@esprit.tn>
2023-11-09 14:37:01 +0100

    ajout main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
+#include "mainwindow.h"
 
-using namespace std;
-
-int main()
+#include <QApplication>
+#include<QMessageBox>
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!AZIZ2023" << endl;
-    
-    return 0;
+    QApplication a(argc, argv);
+    Connexion c;
+    bool test=c.createconnect();
+    Login *loginlab = new Login;
+
+    if(test)
+    {
+        loginlab->show();
+         QMessageBox::information(nullptr,QObject::tr("database is open"),QObject::tr("connection successful.\n""Click cancel to exit."),QMessageBox::Cancel);
+
+    }
+    else
+    {
+        QMessageBox::critical(nullptr,QObject::tr("database is not open"),QObject::tr("connection failed.\n""Click cancel to exit."),QMessageBox::Cancel);
+    }
+
+    return a.exec();
 }
